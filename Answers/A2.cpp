c7d8960e81ce0db024d7eacdad8d90f7d2f9d604
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2020-03-10 20:31:30 +0530

    Create A2.cpp

---
+++ Answers/A2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    long b = 12345678912345;
+    char ch = 'a';
+    float c = 334.230 ;
+    double d = 14049.304930000;
+    printf("%d\n%ld\n%c\n%f\n%lf", a,b,ch,c,d);
+    return 0;
+}
